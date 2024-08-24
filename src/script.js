7aac285c2d7a0795dc01ffaae733dc9fdff3be58
Vinncent Miller <vinncentmiller@gmail.com>
2024-08-24 18:54:11 -0400

    Added falling to pieces

--- src/script.js
+++ src/script.js
@@ -1,13 +1,26 @@
+const canvas = document.getElementById("gameBoard");
+let speed = 100;
+let onBoard = [];
+let activePiece;
+
 // make field
 function drawBoard() {
-    const canvas = document.getElementById("gameBoard");
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d");
-        drawShape(ctx);
-    } else {
-        // canvas not supported
-        console.log("Canvas not supported.")
+    let xIndex = 0;
+    let yIndex = -1;
+    for (let i = 0; i < 200; i++) {
+        if (yIndex != 0) {
+            if (yIndex % 9 == 0) {
+                xIndex++;
+                yIndex = -1;
+            }
+        }
+        yIndex++;
+        const div = document.createElement("div");
+        div.id = [xIndex, yIndex];
+        div.setAttribute("filled", false)
+        canvas.appendChild(div);
     }
+    //getTetro();
 }
 
 
@@ -15,40 +28,53 @@ function drawBoard() {
 // generate random tetrominoes
 // probably want to move the piece randomization to another function
 // so that i can put it in hold later 
-function drawShape(ctx) {
-    const xValues = [0, 30, 60, 90, 120, 150, 180, 210]
-    let x = xValues[Math.floor(Math.random() * 9)];
+function getTetro() {
+    let x = Math.floor(Math.random() * 9);
     const y = 0; // always 0 to start at top of board
-    const shapes = [
-        {name: "I", drawing: [[x, y, 30, 30],[x, y + 30, 30, 30],[x, y + 60, 30, 30],[x, y + 90, 30, 30],], color: "#01EDFA"},
-        {name: "J", drawing: [[x + 30, y, 30, 30],[x + 30, y + 30, 30, 30],[x + 30, y + 60, 30, 30],[x, y + 60, 30, 30],], color: "#485DC5"},
-        {name: "L", drawing: [[x, y, 30, 30],[x, y + 30, 30, 30],[x, y + 60, 30, 30],[x + 30, y + 60, 30, 30],], color: "#FF910C"},
-        {name: "Sq", drawing: [[x, y, 30, 30],[x, y + 30, 30, 30],[x + 30, y, 30, 30],[x + 30, y + 30, 30, 30],], color: "#FEFB34"},
-        {name: "BZ", drawing: [[x, y + 30, 30, 30],[x + 30, y + 30, 30, 30],[x + 30, y, 30, 30],[x + 60, y, 30, 30],], color: "#53DA3F"},
-        {name: "Z", drawing: [[x +30, y + 30, 30, 30],[x + 60, y + 30, 30, 30],[x + 30, y, 30, 30],[x, y, 30, 30],], color: "#EA141C"},
-        {name: "T", drawing: [[x +30, y + 30, 30, 30],[x + 60, y, 30, 30],[x + 30, y, 30, 30],[x, y, 30, 30],], color: "#DD0AB2"},
+    //might make negative to start off board 
+    const tetros = [
+        {name: "I", drawing: [[x, y],[x, y + 1],[x, y + 2],[x, y + 3]], color: "#01EDFA"}, //cyan
+        {name: "J", drawing: [[x + 1, y],[x + 1, y + 1],[x + 1, y + 2],[x, y + 2],], color: "#485DC5"}, // blue
+        {name: "L", drawing: [[x, y],[x, y + 1],[x, y + 2],[x + 1, y + 2],], color: "#FFC82E"}, // orange
+        {name: "O", drawing: [[x, y],[x, y + 1],[x + 1, y],[x + 1, y + 1],], color: "#FEFB34"}, // yellow
+        {name: "S", drawing: [[x, y],[x, y + 1],[x + 1, y + 1],[x + 1, y + 2],], color: "#53DA3F"}, // green
+        {name: "Z", drawing: [[x + 1, y],[x + 1, y + 1],[x, y+1],[x, y+2],], color: "#FD3F59"}, // red
+        {name: "T", drawing: [[x, y],[x, y + 1],[x, y + 2],[x + 1, y + 1]], color: "#DD0AB2"}, // purple
     ];
 
     let piece = Math.floor(Math.random() * 7);
-    shapes[piece].drawing.forEach(square => {
-        ctx.fillStyle = shapes[piece].color;
-        ctx.strokeStyle = "#2E2E84"
-        ctx.fillRect(square[0], square[1], square[2], square[3])
-        ctx.strokeRect(square[0], square[1], square[2], square[3])
-    });
-
+    //To Do: checkIfFilled
+    // if (checkIfFilled()) {
+    //     // lose game
+    // } else {
+        onBoard.push(activePiece)
+        activePiece = setInterval(activePieceFall, speed, tetros[piece])
+    // }
 }
-// pieces:
 
-// T: purple
-// [][][]
-//   []
-// Z: red
-// [][]
-//   [][]
+function drawShape(piece) {
+     piece.drawing.forEach(square => {
+        let eachBlock = document.getElementById(`${square[1]},${square[0]}`);
+        eachBlock.style.backgroundColor=piece.color;
+        eachBlock.setAttribute("filled", true)
+     });
+}
 
 // move tetro from top to bottom
-
+function activePieceFall(piece) {
+    piece.drawing.forEach(square => {
+        // clear square
+        let prevBlock = document.getElementById(`${square[1]},${square[0]}`);
+        prevBlock.style.backgroundColor="transparent";
+        prevBlock.setAttribute("filled", false)
+        square[1]++; 
+        drawShape(piece);
+        if (square[1] >= 19) {
+            clearInterval(activePiece)
+            getTetro();
+        }
+    });
+}
 
 
 // when land, check for win condition
